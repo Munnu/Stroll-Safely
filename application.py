e4bd104e03db7cddafabfd8a2c1b91e97ef3f3ee
munnu <moniqueblake4@gmail.com>
2016-05-11 12:30:03 -0700

    Flaskifying and adding requirements.txt

---
+++ application.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """ runs app name mainspace """
+    return render_template("main.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
