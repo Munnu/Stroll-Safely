aeda2a00e790e853ec973b195e31ef4ec695a8b6
munnu <moniqueblake4@gmail.com>
2016-05-13 08:07:19 -0700

    Working to include 20 data points from db

--- application.py
+++ application.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for, jsonify
 from flask_restful import marshal_with, fields
 from model import Crime_Data_NYC, connect_to_db
+#from model import init_app
+
+from middle import get_twenty
 
 app = Flask(__name__)
 
@@ -8,6 +11,8 @@ app = Flask(__name__)
 
 mfields = {'type': fields.Raw,
             'features': fields.Raw}
+
+
 @marshal_with(mfields)
 def construct():
     constructed_json = {'type': 'FeatureCollection',
@@ -18,10 +23,9 @@ def construct():
                                 'coordinates': [40.7282239, -73.79485160000002]
                                 },
                                 'properties': {
-                                'prop0': 'value0'
+                                    'prop0': 'value0'
                                 }
-                        }
-                    ]}
+                        }]}
     return constructed_json
 
 
@@ -30,26 +34,27 @@ def index():
     """ runs app name mainspace """
     return render_template("main.html")
 
+
 @app.route('/geojson_sample.json')
 def geojson_sample():
     """ return a geojson """
     return jsonify(construct())
 
+
 @app.route('/crimes.json')
 def crimes():
     """ return a json of crimes lat, longitude """
-    crimes_coords = {'crimes': [
-            {'latitude': 40.7127837, 'longitude': -74.00594130000002},
-            {'latitude': 40.57948579, 'longitude': -73.99844033},
-            {'latitude': 40.83069115, 'longitude': -73.95586724}
-            ]}
-    
+    # crimes_coords = {'crimes': [
+    #         {'latitude': 40.7127837, 'longitude': -74.00594130000002},
+    #         {'latitude': 40.57948579, 'longitude': -73.99844033},
+    #         {'latitude': 40.83069115, 'longitude': -73.95586724}
+    #         ]}
+    crimes_coords = get_twenty()
     return jsonify(crimes_coords)
 
 if __name__ == '__main__':
-    app.run(debug=True)
     app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
     connect_to_db(app)
-    app.run()
+    app.run(debug=True)
--- middle.py
+++ middle.py
@@ -1,7 +1,9 @@
 import json
-from model import Crime_Data_NYC, connect_to_db, db
-from application import app
+from model import Crime_Data_NYC, connect_to_db, db, init_app
+#from application import app
 from gmaps import Directions
+
+init_app()
 
 api = Directions()
 
@@ -12,16 +14,34 @@ results = api.directions((40.728783, -73.7897503),
 
 # print json.dumps(results, indent=2)
 
+
 def get_twenty():
     """ Test to see if I could get a list of 20 items from db """
+
     twenty_entries = Crime_Data_NYC.query.limit(20).all()
-    print "This is twenty entries", twenty_entries
+
+    crimes_coords = {'crimes': []}
+
+    for entry in twenty_entries:
+        # get the location in string format of "(0, 0)"
+        # and other nasty string to float conversion stuff here
+        location = entry.location[1:-1].split(",")
+        location_lat = float(location[0].strip())
+        location_lng = float(location[1].strip())
+
+        format_loc_dict = {'latitude': location_lat, 'longitude': location_lng}
+
+        # append to crimes_coords inner list
+        crimes_coords['crimes'].append(format_loc_dict)
+
+    return crimes_coords
 
 
+# if __name__ == "__main__":
+#     connect_to_db(app)
 
-if __name__ == '__main__':
-    app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+#     # In case tables haven't been created, create them
+#     db.create_all()
 
-    connect_to_db(app)
-    get_twenty()
+#     # Import different types of data
+#     get_twenty()
